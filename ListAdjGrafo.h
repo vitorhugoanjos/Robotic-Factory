6eb41b0197a779d53df8c51844d265e12abc34c9
Vitor Hugo <vitorhugoanjosowns@gmail.com>
2012-11-21 15:35:10 +0000

    ListaAdj

--- ListAdjGrafo.h
+++ ListAdjGrafo.h
@@ -3,8 +3,7 @@
 
 #include "Vertice.h"
 #include "Ramo.h"
-#include "Queue.h"
-#include "Stack.h"
+
 
 template<class TV,class TR>
 class ListAdjGrafo
@@ -14,12 +13,9 @@ private:
 	int nvertices;
 	int nramos;
 	Vertice<TV,TR>* graf;
-	void visita_profundidade(Vertice<TV,TR>* apinicio, int *vector) const;
-	void mostra_Cam(Vertice<TV,TR>* apinicio, Vertice<TV,TR>* apfim, int *vector, Stack<TV> st);
-	void mostraCaminho(int origem, int destino, const int *caminho);
-	int distanciaMinimoVertice(int *distancia, bool *processado) const;
-	void caminhominimoP(Vertice<TV,TR> *inicio, Vertice<TV,TR> *fim);
-	void mostracaminhos(Vertice<TV,TR> *apinicio,Vertice<TV,TR> *apfim, int *vector, Stack<TV>s, TR &custo, TR &custlim);
+
+	bool Caminho(Vertice <TV,TR>*v1,Vertice <TV,TR>*v2) const;
+	bool Caminho(Vertice <TV,TR>*v1,Vertice <TV,TR>*v2, int *vec) const;
 public:
 	ListAdjGrafo();
 	ListAdjGrafo(const ListAdjGrafo<TV,TR>& G) ;
@@ -27,6 +23,7 @@ public:
 
 	Vertice<TV,TR>* encvert_conteudo(const TV& v) const;
 	Vertice<TV,TR>* encvert_key(int numvert) const;
+	Vertice<TV,TR>* encvert_keyPosto(int k) const;
 
 	int NumVert() const ;
 	int NumRamos() const ;
@@ -41,14 +38,75 @@ public:
 
 	void escreve_grafo() const ;
 
-	void visita_Largura(const TV& vinicio) const;
-	void visita_Prof(const TV& vinicio) const;
-
-	void mostra_caminhos(const TV& vinicio, const TV& vfim);
-	void caminhoMinimoPesado(const TV &inicio, const TV &fim);
-	int tempovisitatodassalas()const;
-	void mostraCaminhosPossiveis(const TV &inicio,const TV &fim);
+	bool Caminho(const TV &v1,const TV &v2) const;
+	bool Conexo() const;
 };
+
+
+
+// Método Caminho privado de pesquisa em profundidade recursivo
+template<class TV,class TR>
+bool ListAdjGrafo<TV,TR>::Caminho(Vertice <TV,TR>*v1,	Vertice <TV,TR>*v2, int *vec) const
+{
+	vec[v1->key] = 1;
+	if (v1==v2) return true;
+
+	Ramo <TV,TR> *ramo = v1->apramo;
+	while (ramo !=NULL)
+	{
+		if ((vec[ramo->apv->key]==0) && Caminho(ramo->apv, v2,vec)) return true;
+		ramo = ramo->apr;
+	}
+	return false;
+}
+
+// Método Caminho privado para inicializar o vector (que evita ciclos) em cada chamada ao método de pesquisa em profundidade recursivo
+template<class TV,class TR>
+bool ListAdjGrafo<TV,TR>::Caminho(Vertice <TV,TR>*v1, Vertice <TV,TR>*v2) const
+{
+	int *vec = new int[nvertices+1];
+	for (int i =1; i <=nvertices; i++) vec[i]=0;
+	return Caminho(v1,v2,vec);
+}
+
+
+
+// Método existeCaminho público, recebe os vértices em TV, converte-os em apontadores de vertice e chama o método privado
+template<class TV,class TR>
+bool ListAdjGrafo<TV,TR>::Caminho(const TV &v1,const TV &v2) const
+{
+	Vertice <TV,TR> *apv1, *apv2;
+	apv1 = encvert_conteudo(v1);
+	apv2 = encvert_conteudo(v2);
+
+	return Caminho(apv1,apv2);
+}
+
+
+// Método conexo, gera todos os pares de vértices e aplica a definição de conexidade
+template<class TV,class TR>
+bool ListAdjGrafo<TV,TR>::Conexo() const
+{
+	if (graf==NULL) return true;
+
+	Vertice <TV,TR> *v1, *v2;
+	v1 = graf;
+
+	while (v1->apvertice != NULL)
+	{
+		v2 = v1->apvertice;
+		while (v2 != NULL)
+		{
+			if (!Caminho(v1,v2) && !Caminho(v2,v1))
+				return false;
+			v2 = v2->apvertice;
+		}
+		v1=v1->apvertice;
+	}
+
+	return true;
+}
+
 
 template<class TV,class TR>
 ListAdjGrafo<TV,TR>::ListAdjGrafo()
@@ -217,6 +275,18 @@ Vertice<TV,TR>* ListAdjGrafo<TV,TR>::encvert_key(int numvert) const
 }
 
 template<class TV,class TR>
+Vertice<TV,TR>* ListAdjGrafo<TV,TR>::encvert_keyPosto(int k ) const{
+	Vertice<TV,TR>* ap=graf;
+	while(ap!=NULL){
+		if(ap->vconteudo->getKey() == k)
+			return ap;
+		else
+			ap=ap->apvertice;
+	}
+	return ap;
+}
+
+template<class TV,class TR>
 void ListAdjGrafo<TV,TR>::juntar_vertice(const TV& vert)
 {
 	if (nvertices == 0)
@@ -325,7 +395,7 @@ int ListAdjGrafo<TV,TR>::grau_saida (const TV& vert) const
 	Ramo<TV,TR>* ap1;
 	int grau = 0 ;
 
-	if (ap)
+	if (ap->vconteudo == vert)
 	{
 		ap1=ap->apramo;
 		while (ap1)  
@@ -333,6 +403,7 @@ int ListAdjGrafo<TV,TR>::grau_saida (const TV& vert) const
 			grau++ ;
 			ap1=ap1->apr ; 
 		}
+		ap=ap->apvertice ;
 	}
 	return grau ;
 }
@@ -347,8 +418,8 @@ void ListAdjGrafo<TV,TR>::escreve_grafo() const
 		cout << "Grafo nao definido !" << endl ; 
 	else
 	{
-		cout << "Num vertices " << nvertices  << endl ; 
-		cout << "Num ramos " << nramos  << endl ; 
+		cout << "Numero de vertices: " << nvertices  << endl ; 
+		cout << "Numero de ramos " << nramos  << endl ; 
 
 		while (v != NULL)
 		{
@@ -366,264 +437,6 @@ void ListAdjGrafo<TV,TR>::escreve_grafo() const
 	}
 }
 
-template<class TV, class TR>
-void ListAdjGrafo<TV,TR>::visita_Largura(const TV& vinicio) const
-{
-	Vertice<TV,TR>* apinicio = encvert_conteudo(vinicio);
-	int *vetor=new int [nvertices+1];
-	for (int i = 0; i <= nvertices; i++)
-	{
-		vetor[i]=0;
-	}
-	if(apinicio!=NULL){
-		Queue<Vertice<TV,TR>*> q;
-		Ramo<TV,TR> *apramo;
-		cout << apinicio->vconteudo;
-		vetor[apinicio->key]=1;
 
-		q.insere(apinicio);
-		while(!q.vazia())
-		{
-			q.retira(apinicio);
-			apramo=apinicio->apramo;
-			while(apramo!=NULL)
-			{
-				int outrovert=apramo->apv->key;
-				if(vetor[outrovert] == 0)
-				{
-					vetor[outrovert]=1;
-					cout << apramo->apv->vconteudo;
-					q.insere(apramo->apv);
-				}
-				apramo=apramo->apr;
-			}	
-		}
-	}else
-	{
-		cout << "Vertice nao encontrado";
-	}
-	cout << endl;
-}
-
-template<class TV, class TR>
-void ListAdjGrafo<TV,TR>::visita_Prof(const TV& vinicio) const
-{
-	Vertice<TV,TR> *apinicio = encvert_conteudo(vinicio);
-	if(apinicio!=NULL)
-	{
-		int *vector=new int[nvertices+1];
-		for (int i = 0; i <= nvertices; i++)
-		{
-			vector[i]=0;
-		}
-		visita_profundidade(apinicio, vector);
-	}else
-		cout << "Vertice nao encontrado";
-	cout << endl;
-}
-
-template<class TV, class TR>
-void ListAdjGrafo<TV,TR>::visita_profundidade(Vertice<TV,TR>* apinicio, int *vector) const
-{
-	Ramo<TV,TR> *apramo;
-	vector[apinicio->key]=1;
-	cout << apinicio->vconteudo;
-	apramo=apinicio->apramo;
-	while(apramo!=NULL)
-	{
-		int outrovert=apramo->apv->key;
-		if(vector[outrovert]==0)
-			visita_profundidade(apramo->apv,vector);
-		apramo=apramo->apr;
-	}
-}
-
-template <class TV, class TR>
-void ListAdjGrafo<TV,TR>::mostra_caminhos(const TV& vinicio,const TV& vfim)
-{
-	Vertice<TV,TR> *apinicio = encvert_conteudo(vinicio);
-	Vertice<TV,TR> *apfim = encvert_conteudo(vfim);
-	if(apinicio!=NULL && apfim!=NULL)
-	{
-		int *vector=new int[nvertices+1];
-		for (int i= 0; i<=nvertices; i++)
-			vector[i]=0;
-		Stack<TV> st;
-		mostra_Cam(apinicio,apfim,vector,st);	
-	}else
-		cout << "Vertice nao encontrado";
-	cout << endl;
-
-
-}
-
-template<class TV, class TR>
-void ListAdjGrafo<TV,TR>::mostra_Cam(Vertice<TV,TR>* apinicio, Vertice<TV,TR>* apfim, int *vector, Stack<TV> st)
-{
-	vector[apinicio->key]=1;
-	st.push(apinicio->vconteudo);
-	Ramo<TV,TR> *apramo=apinicio->apramo;
-	TV vert;
-	while(apramo!=NULL)
-	{
-		if (apramo->apv->vconteudo == apfim->vconteudo)
-		{
-			st.push(apramo->apv->vconteudo);
-			//inverter
-			Stack<TV> aux;
-			while(!st.vazia())
-			{
-				st.pop(vert);
-				aux.push(vert);
-			}
-			cout << aux;
-			cout << endl;
-			while(!aux.vazia())
-			{
-				aux.pop(vert);
-				st.push(vert);
-			}
-			st.pop(vert);
-		}else if(vector[apramo->apv->key]==0)
-			mostra_Cam(apramo->apv,apfim,vector,st);
-		apramo=apramo->apr;
-	}
-	vector[apinicio->key]=0;
-	st.pop(vert);
-}
-
-template<class TV, class TR>
-int ListAdjGrafo<TV,TR>::distanciaMinimoVertice(int *distancia, bool *processado) const 
-{
-	int minimo=INT_MAX,indvertice=-1;
-	for (int i = 1; i < nvertices; i++)
-	{
-		if(!processado[i]&&distancia[i]<minimo)
-		{
-			minimo=distancia[i];
-			indvertice=i;
-		}
-	}
-	return indvertice;
-}
-
-template<class TV, class TR>
-void ListAdjGrafo<TV,TR>::mostraCaminho(int origem, int destino, const int *caminho)
-{
-	if(origem!=destino){
-		mostraCaminho(origem,caminho[destino],caminho);
-		cout << "--";
-	}
-	Vertice<TV,TR>* apvert=encvert_key(destino);
-	cout << apvert->vconteudo;
-}
-
-template<class TV,class TR>
-void ListAdjGrafo<TV,TR>::caminhoMinimoPesado(const TV &inicio, const TV &fim)
-{
-	Vertice<TV,TR>* apvini=encvert_conteudo(inicio);
-	Vertice<TV,TR>* apvfim=encvert_conteudo(fim);
-	if(apvini==NULL || apvfim==NULL)
-		cout << "Vertices nao encontrados" << endl;
-	else
-	{
-		caminhominimoP(apvini, apvfim);	
-		cout << endl;
-	}
-}
-
-template<class TV, class TR>
-void ListAdjGrafo<TV,TR>::caminhominimoP(Vertice<TV,TR> *inicio,Vertice<TV,TR> *fim){
-	bool *processados=new bool[nvertices+1];
-	int *distancia = new int [nvertices+1];
-	int *caminho= new int [nvertices+1];
-	Vertice<TV,TR> *ini=inicio;
-	Vertice<TV,TR> *f=fim;
-	for (int i = 1; i <= nvertices;++i)
-	{
-		processados[i]=false;
-		caminho[i]=0;
-		distancia[i]=9999;
-	}
-	int indOrg=inicio->key;
-	distancia[inicio->key]=0;
-	while(indOrg!=-1){
-		processados[indOrg]=true;
-		inicio=encvert_key(indOrg);
-		Ramo<TV,TR>* apramo=inicio->apramo;
-		while(apramo!=NULL){
-			int indDest=apramo->apv->key;
-			if(!processados[indDest]&&distancia[indDest]>distancia[indOrg]+apramo->rconteudo){
-				distancia[indDest]=distancia[indOrg]+apramo->rconteudo;
-				caminho[indDest]=indOrg;
-			}
-			apramo=apramo->apr;
-		}
-		indOrg=distanciaMinimoVertice(distancia,processados);
-	}
-	if(distancia[f->key]==9999){
-		cout << "nao existe caminho entre " << ini->vconteudo << " e " << f->vconteudo<< endl;
-	}else{
-		mostraCaminho(ini->key, f->key, caminho);
-	}
-}
-
-template<class TV,class TR>
-int ListAdjGrafo<TV,TR>::tempovisitatodassalas()const
-{
-	Vertice<TV,TR>	*ap=graf;
-	int temptot=0;
-	while(ap!=NULL)
-	{
-		temptot+=ap->vconteudo.getTempo();
-		ap=ap->apvertice;	
-	}
-	return temptot;
-}
-
-template<class TV,class TR>
-void ListAdjGrafo<TV,TR>::mostracaminhos(Vertice<TV,TR> *apinicio,Vertice<TV,TR> *apfim, int *vector, Stack<TV>s, TR &custo, TR &custlim)
-{
-	TV nvdest;
-	Ramo<TV,TR> *apramo;
-	Stack <TV> cam;
-	
-	if(vector[apinicio->vconteudo.getNum()==0])
-		vector[apinicio->vonteudo.getNum()]=1;
-	s.push(apinicio->vconteudo.getNum());
-	apramo=apinicio->apramo;
-	while(apramo!=NULL)
-	{
-		nvdest=apramo->apv->vonteudo.getNum();
-		if(nvdest==apfim->vconteudo.getNum())
-		{
-			custo+=apfim->vconteudo->getTempo();
-			s.push(nvdest);
-			if(custo<=custolim
-			{
-				cam=s;
-				cam.inverte();
-				cout<<cam<<":"<<custo<<endl;
-			}
-			s.pop(nvdest);
-			custo-=apfim->vconteudo.getTempo();
-		}else if(vector[nvdest]==0)
-		{
-			custo+=apramo->apv->vconteudo.getTempo();
-			vector[nvdest]=apramo->apv->vconteudo.getTempo();
-			mostracaminhos(apramo->apv,apfim,vector,s,custo,custolim);		
-		}
-		apramo=apramo->apr;	
-	}
-	custo-=vector[apinicio->vconteudo.getTempo()];
-	vector[apinicio->vconteudo.getNum()]=0;	
-}
-
-template<class TV,class TR>
-void ListAdjGrafo<TV,TR>::mostraCaminhosPossiveis(const TV &inicio,const TV &fim)
-{
-	Vertice<TV,TR> *apinicio = encvert_conteudo(
-
-}
 
 #endif
